6d01f3789a359ff789e664898f2defe52dfe8347
sneh.ghetiya <sneh.ghetiya@openxcell.com>
2024-02-01 09:21:32 +0530

    changes in layout

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -1,22 +1,60 @@
 /* eslint-disable @next/next/no-img-element */
 
+import { AppTopbarRef, LayoutConfig } from '@/types';
 import Link from 'next/link';
+import { PrimeReactContext } from 'primereact/api';
+import { InputSwitch, InputSwitchChangeEvent } from 'primereact/inputswitch';
 import { classNames } from 'primereact/utils';
-import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
-import { AppTopbarRef } from '@/types';
+import { forwardRef, useContext, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react';
 import { LayoutContext } from './context/layoutcontext';
 
-const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
-    const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
+const AppTopbar = forwardRef<AppTopbarRef>((_props, ref) => {
+    const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar, setLayoutConfig } = useContext(LayoutContext);
     const menubuttonRef = useRef(null);
     const topbarmenuRef = useRef(null);
     const topbarmenubuttonRef = useRef(null);
+    const { setRipple, changeTheme } = useContext(PrimeReactContext);
+    const [isToggled, setIsToggled] = useState(false);
+
+    useLayoutEffect(() => {
+        if (typeof window !== 'undefined') {
+            setIsToggled(() => JSON.parse(window.localStorage.getItem('isToggled') ?? 'false'));
+        }
+    }, []);
+
+    useLayoutEffect(() => {
+        if (typeof window !== 'undefined' && JSON.parse(window.localStorage.getItem('isToggled') ?? 'false')) {
+            _changeTheme('lara-dark-purple', 'dark');
+        } else {
+            _changeTheme('lara-light-purple', 'light');
+        }
+    }, []);
 
     useImperativeHandle(ref, () => ({
         menubutton: menubuttonRef.current,
         topbarmenu: topbarmenuRef.current,
         topbarmenubutton: topbarmenubuttonRef.current
     }));
+
+    const changeRipple = (e: InputSwitchChangeEvent) => {
+        setRipple?.(e.value as boolean);
+        setLayoutConfig((prevState: LayoutConfig) => ({ ...prevState, ripple: e.value as boolean }));
+        if (e?.target?.value === true) {
+            setIsToggled(() => true);
+            window.localStorage.setItem('isToggled', JSON.stringify(true));
+            _changeTheme('lara-dark-purple', 'dark');
+        } else {
+            setIsToggled(() => false);
+            window.localStorage.setItem('isToggled', JSON.stringify(false));
+            _changeTheme('lara-light-purple', 'light');
+        }
+    };
+
+    const _changeTheme = (theme: string, colorScheme: string) => {
+        changeTheme?.(layoutConfig.theme, theme, 'theme-css', () => {
+            setLayoutConfig((prevState: LayoutConfig) => ({ ...prevState, theme, colorScheme }));
+        });
+    };
 
     return (
         <div className="layout-topbar">
@@ -35,19 +73,14 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
 
             <div ref={topbarmenuRef} className={classNames('layout-topbar-menu', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
                 <button type="button" className="p-link layout-topbar-button">
-                    <i className="pi pi-calendar"></i>
-                    <span>Calendar</span>
-                </button>
-                <button type="button" className="p-link layout-topbar-button">
                     <i className="pi pi-user"></i>
                     <span>Profile</span>
                 </button>
-                <Link href="/documentation">
-                    <button type="button" className="p-link layout-topbar-button">
-                        <i className="pi pi-cog"></i>
-                        <span>Settings</span>
-                    </button>
-                </Link>
+
+                <div className={classNames('p-link layout-topbar-toggle-container', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
+                    <InputSwitch checked={!!isToggled as boolean} onChange={(e) => changeRipple(e)}></InputSwitch>
+                    <span className="theme-span">Toggle Theme</span>
+                </div>
             </div>
         </div>
     );
